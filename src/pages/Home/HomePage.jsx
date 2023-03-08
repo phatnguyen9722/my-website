86506cab670d9765bd200dd085825b63281c6469
ntanphat2 <ntanphat2@tma.com>
2023-03-08 13:51:58 +0700

    build problem

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 import Helmet from '../../components/common/Helmet';
 import Section, { SectionTitle, SectionBody } from '../../components/common/Section';
